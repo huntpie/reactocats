1e059374a3c36eaa0758de3a10687bf5dd54c05f
Pierce Hunt <huntpie@gmail.com>
2021-02-18 11:20:53 -0500

    Two cats added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React, { Component } from 'react'
+import { Terracottocat } from './components/Terracottocat'
+import { Octogatos } from './components/Octogatos'
+
 
 export class App extends Component {
   render() {
-    return <div>Hello, World!</div>
+    return (
+    <body className="bg-gray-light">
+      <header className="header mb-6">
+        <div className="container px-4">
+          <nav className="d-flex flex-items-center text-grey"></nav>
+        </div>
+      </header> 
+      <main>
+        <Terracottocat />
+        <Octogatos />
+      </main>
+    </body>
+    )
   }
 }
